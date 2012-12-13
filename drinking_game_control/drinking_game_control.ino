c345f93e0fc622e4d1b3b8efa822fa62661d8e83
Steve Faletti <stevefaletti@gmail.com>
2012-12-13 01:35:23 -0500

    bit-shifting fun

--- drinking_game_control/drinking_game_control.ino
+++ drinking_game_control/drinking_game_control.ino
@@ -19,7 +19,8 @@ int player1Val, player2Val;
 
 byte disp1Val = 0;
 byte disp2Val = 0;
-byte score1Val, score2Val;
+byte score1Val = B00000000;
+byte score2Val = B00001000;
 byte scoreVal = 0;
 
 
@@ -40,12 +41,10 @@ void loop(){
   player2Val = byte(random(4, 8)); //generate random value for player2
   byte gameVal = 1 << player1Val | 1 << player2Val; //save play pins in a byte
 
-  
-
   // blink player1 and player2 lights using random values
   digitalWrite(REG_PIN, LOW);
   digitalWrite(CLOCK_PIN, LOW);
-  for (byte mask = 00000001; mask > 0; mask <<= 1){
+  for (byte mask = B00000001; mask > 0; mask <<= 1){
     digitalWrite(GAME_PIN, gameVal & mask);
     digitalWrite(SCORE_PIN, scoreVal & mask);
     digitalWrite(DISP1_PIN, disp1Val & mask);
@@ -60,11 +59,14 @@ void loop(){
     break; //no winner, rerun button scan
   case 1:
     //TODO: player 1 wins
+    score1Val <<= 1; //this is wrong
     break;
   case 2:
-    //TODO: player 2 wins
+    //TODO: player 2 wins          
+    score2Val <<= 1; //this is wrong
     break;
   }
+  scoreVal = score1Val | score2Val; //this is wrong
 }
 
 int winnerVal(int _player1, int _player2){ //scrub buttons and check which was pressed
@@ -74,12 +76,10 @@ int winnerVal(int _player1, int _player2){ //scrub buttons and check which was p
       if (i == player1Val || i == player2Val){ //check that button pressed equals the target
         if(i < 4){
           winVal = 1; //player1 wins
-          score1Val ++;
           break;
         }
         if(i >= 4){
           winVal = 2; //player2 wins
-          score2Val ++;
           break;
         }
       }
@@ -88,6 +88,7 @@ int winnerVal(int _player1, int _player2){ //scrub buttons and check which was p
   }
   return winVal;
 }
+
 
 
 
